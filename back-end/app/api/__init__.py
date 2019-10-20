b58441fe9189aad4f5ae09f314130f3483dd2753
shui <164635470@qq.com>
2019-10-20 23:05:55 +0800

    博客curd后端

--- back-end/app/api/__init__.py
+++ back-end/app/api/__init__.py
@@ -3,4 +3,5 @@ from flask import Blueprint
 bp = Blueprint('api', __name__)
 
 
-from  app.api import ping,users,tokens
+# 写在最后是为了防止循环导入，ping.py文件也会导入 bp
+from  app.api import ping,users,tokens, posts
--- back-end/app/models.py
+++ back-end/app/models.py
@@ -44,6 +44,10 @@ class User(PaginatedAPIMixin, db.Model):
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
+    # 反向引用，直接查询出当前用户的所有博客文章; 同时，Post实例中会有 author 属性
+    # cascade 用于级联删除，当删除user时，该user下面的所有posts都会被级联删除
+    posts = db.relationship('Post', backref='author', lazy='dynamic', cascade='all, delete-orphan')
+
     #token
     # token = db.Column(db.String(32), index=True, unique=True)
     # token_expiration = db.Column(db.DateTime)
@@ -132,3 +136,54 @@ class User(PaginatedAPIMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+
+
+class Post(PaginatedAPIMixin, db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    summary = db.Column(db.Text)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    views = db.Column(db.Integer, default=0)
+
+    # 外键, 直接操纵数据库当user下面有posts时不允许删除user，下面仅仅是 ORM-level “delete” cascade
+    # db.ForeignKey('users.id', ondelete='CASCADE') 会同时在数据库中指定 FOREIGN KEY level “ON DELETE” cascade
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # author = db.relationship('User', backref='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
+
+    def to_dict(self):
+        data = {
+            'id': self.id,
+            'title': self.title,
+            'summary': self.summary,
+            'body': self.body,
+            'timestamp': self.timestamp,
+            'views': self.views,
+            'author': self.author.to_dict(),
+            '_links': {
+                'self': url_for('api.get_post', id=self.id),
+                'author_url': url_for('api.get_user', id=self.author_id)
+            }
+        }
+        return data
+
+    def from_dict(self,data):
+        for field in ['title','summary','body']:
+            if field in data:
+                setattr(self, field, data[field])
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        '''
+        target: 有监听事件发生的 Post 实例对象
+        value: 监听哪个字段的变化
+        '''
+        if not target.summary:  # 如果前端不填写摘要，是空str，而不是None
+            target.summary = value[:200] + '  ... ...'  # 截取 body 字段的前200个字符给 summary
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)  # body 字段有变化时，执行 on_changed_body() 方法
